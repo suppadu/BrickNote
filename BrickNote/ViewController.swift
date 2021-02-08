81dfdd158cb55bfa318d4ac43581aa094be4376a
Suppadu <dkon1411@gmail.com>
2021-02-08 15:38:14 +0800

    Initial Commit

---
+++ BrickNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrickNote
+//
+//  Created by Дмитрий Константинов on 08.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
